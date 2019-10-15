4ba144e5461ab49dd90e26b92fc84eabce46d7bc
shoamco <shoamc98@gmail.com>
2019-10-15 06:46:50 -0700

    Add documentation to explain the class

--- FixedPoint.h
+++ FixedPoint.h
@@ -12,10 +12,9 @@
 
 
 //T-type
-
+//template<typename T , unsigned char I,unsigned char F>
 /**********************************************
  *             class FixedPoint
- *
   |S-sing bit|I-integer part bit|-F-fractional part bit|
 
  * @tparam T -: type
@@ -23,97 +22,80 @@
  * @tparam F :The fractional part bit
  *
  * ********************************************/
+
+
+template<typename T = int, unsigned int SIZE = 2>
+/************************************************************************************************************
+                             class FixedPoint
+
+ * @tparam T -the data type (for example:int unsigned short int...)
+ * @tparam SIZE -the scale
+
+ fixed-point data type is essentially an integer that is scaled by an implicit specific factor determined by the type
+ *  For example, the value 1.23 can be represented as 1230 in a fixed-point data type with scaling factor of 1/1000
+    and the value 1,230,000 can be represented as 1230 with a scaling factor of 1000
+
+ *   Unlike floating-point data types, the scaling factor is the same for all values of the same type ,
+    and does not change during the entire computation.
+
+
+ *   To add or subtract two values of the same fixed-point type: it is sufficient to add or subtract the underlying
+    integers, and keep their common scaling factor,The result can be exactly represented in the same type(as long as no
+    overflow occurs)
+    for example:
+    1.23 + 2.43 ==> 123/100 + 243/100 ==>  (123+243)/100 ==> 366/100 = 3.66
+
+
+ * To multiply two fixed-point numbers, it suffices to multiply the two underlying integers, and assume that the scaling
+   factor of the result is the product of their scaling factors ,the result may have to be rounded
+   (in this case the result may have to be rounded, and overflow may occur)
+   for example:
+    1.23 * 2.43 ==>123/100 * 243/100 ==>  (123*243)/(100*100) ==> 29889 /1000 ==> rounded 298/100 =2.89
+
+ * To divide two fixed-point numbers: divide the two underlying integers, and divide the scaling
+   factor,for a more accurate result multiply the first by the scale\SIZE.
+   the result may have to be rounded
+   (in this case the result may have to be rounded, and overflow may occur)
+   for example:
+    4.52 / 1.25 ==>(452/100) / (125/100) ==> (for a more accurate result multiply the first by the scale)
+    (45200/10000) / (125/100) ==>  45200/125 =361.6   (1/10000  / 1/100 ) =100 ==> 361.6/100 ==>
+    rounded  361/100 =3.62
+
+ * *************************************************************************************************************/
+
 class FixedPoint {
 public:
     FixedPoint(T number);
+
 //    FixedPoint(T dollar = 0, T cent = 0, T pip = 0);
-    FixedPoint(const FixedPoint<T,SIZE> &other);
+    FixedPoint(const FixedPoint<T, SIZE> &other);
 
     T GetPrice() const;
 
     template<typename T2>
-    FixedPoint<T,SIZE> &operator=(const FixedPoint<T2> &other);
+    FixedPoint<T, SIZE> &operator=(const FixedPoint<T2> &other);
 
-    FixedPoint<T,SIZE>&operator=(const FixedPoint<T,SIZE> &other);
+    FixedPoint<T, SIZE> &operator=(const FixedPoint<T, SIZE> &other);
 
-    FixedPoint<T,SIZE> &operator=(T dollar);
+    FixedPoint<T, SIZE> &operator=(T dollar);
 
     double GetPriceDollar() const;
+
+    FixedPoint<T, SIZE> GetNumberFixedPoint() const;
 
     /* TemplatePrice &operator=( T dollar);*/
     template<typename U>
     friend std::ostream &operator<<(std::ostream &stream, const FixedPoint<U> &price1);
-//
-///****Binary arithmetic operators***/
-//
-//
-//
-//    // Declare prefix and postfix increment operators.
-//    FixedPoint<T> &operator++();       // Prefix increment operator.
-//    FixedPoint<T> operator++( int);     // Postfix increment operator.
-//
-//    // Declare prefix and postfix decrement operators.
-//    FixedPoint<T> &operator--();       // Prefix decrement operator.
-//    FixedPoint<T> operator--( int);     // Postfix decrement operator
-//     /*unary -*/
-//     FixedPoint<T> operator-() const;
-//
-//    /***operators + ***/
-//    FixedPoint<T> &operator+=(const TemplatePrice<T> &other);
-//
-//    FixedPoint<T>  &operator+=(int val);
-//
-//
-//    TemplatePrice<T> operator+(const   TemplatePrice<T> &other);
-//    TemplatePrice<T> operator+(int val);
-//
-//    /***operators -***/
-//    TemplatePrice<T> &operator-=(const   TemplatePrice<T> &other);
-//
-//    TemplatePrice<T> &operator-=(int val);
-//
-//    TemplatePrice<T> operator-(const   TemplatePrice<T> &other);
-//
-//    TemplatePrice<T> operator-(int val);
-//
-///***   operators *   ***/
-//
-//    TemplatePrice<T> &operator*=(const   TemplatePrice<T> &other);
-//
-//    TemplatePrice<T> &operator*=(int val);
-//
-//    TemplatePrice<T> operator*(const   TemplatePrice<T> &other);
-//
-//    TemplatePrice<T> operator*(int val);
-//
-/////***   operators /   ***/
-//
-//    TemplatePrice<T> &operator/=(const   TemplatePrice<T> &other);
-//
-//    TemplatePrice<T> &operator/=(int val);
-//
-//    TemplatePrice<T> operator/(const   TemplatePrice<T> &other);
-//
-//    TemplatePrice<T> operator/(int val);
-//
-//    /***   operators %   ***/
-//
-//    TemplatePrice<T> &operator%=(const   TemplatePrice<T> &other);
-//
-//    TemplatePrice<T> &operator%=(int val);
-//
-//    TemplatePrice<T> operator%(const   TemplatePrice<T> &other);
-//
-//    TemplatePrice<T> operator%(int val);
-//
-
 
 private:
-    T price;
+
+    int data;// fixed-point data type is  an integer
+
 };
+
 template<typename T, unsigned int SIZE>
 inline
-FixedPoint<T, SIZE>::FixedPoint(T number) : price(number*pow( 10,SIZE )) {
+FixedPoint<T, SIZE>::FixedPoint(T data) : data(data) {
 }
 //template<typename T, unsigned int SIZE>
 //inline
@@ -122,13 +104,13 @@ FixedPoint<T, SIZE>::FixedPoint(T number) : price(number*pow( 10,SIZE )) {
 //}
 
 template<typename T, unsigned int SIZE>
-inline FixedPoint<T, SIZE>::FixedPoint(const FixedPoint<T,SIZE> &other) : price(other.price) {
+inline FixedPoint<T, SIZE>::FixedPoint(const FixedPoint<T, SIZE> &other) : data(other.data) {
 
 }
 
-template<typename T, unsigned int SIZE>
-inline T FixedPoint<T,SIZE>::GetPrice() const {
-    return price;
-}
+//template<typename T, unsigned int SIZE>
+//inline T FixedPoint<T,SIZE>::GetNumberFixedPoint() const {
+//    return number;
+//}
 
 #endif //CPP_FIXED_POINT_SHOAMCO_FIXEDPOINT_H
---
+++ FixedPointException.h
@@ -0,0 +1,23 @@
+
+
+#ifndef CPP_FIXED_POINT_SHOAMCO_FIXEDPOINTEXCEPTION_H
+#define CPP_FIXED_POINT_SHOAMCO_FIXEDPOINTEXCEPTION_H
+
+#include <iostream>
+
+class FixedPointException : public std::runtime_error {
+public:
+    FixedPointException(const std::string &message) : std::runtime_error(message) {}
+};
+
+class OverflowFixedPointException : public FixedPointException {
+public:
+    OverflowFixedPointException() : FixedPointException("Overflow!") {}
+};
+
+class SizeFixedPointException : public FixedPointException {
+public:
+    OverflowFixedPointException() : FixedPointException("The size  bit out of rang.") {}
+};
+
+#endif //CPP_FIXED_POINT_SHOAMCO_FIXEDPOINTEXCEPTION_H
