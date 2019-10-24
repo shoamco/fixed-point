c8676e53b0ef9d176ccca01e5f33ba90be77bb15
shoamco <shoamc98@gmail.com>
2019-10-24 07:13:45 -0700

    Phase IV-add interface/class Color

---
+++ Color.h
@@ -0,0 +1,13 @@
+//
+// Created by shoam on 10/24/19.
+//
+
+#ifndef CPP_FIXED_POINT_SHOAMCO_COLOR_H
+#define CPP_FIXED_POINT_SHOAMCO_COLOR_H
+template <class T>
+Color[] calcFixedPointGradient(T num)
+{
+static Color pallette[num.Precision];
+
+}
+#endif //CPP_FIXED_POINT_SHOAMCO_COLOR_H
